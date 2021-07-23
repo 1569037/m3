e25a20ef2c4b259f5be3ab1dbbd505d8e8907d96
1569037 <evelynramvar26@gmail.com>
2021-07-23 23:18:20 +0200

    Update Vehiculos.java

--- m3/Vehiculos.java
+++ m3/Vehiculos.java
@@ -20,9 +20,36 @@ public abstract class Vehiculos {
 	public Vehiculos(String matricula, String marca, String color)
 	{
 		this();
-		this.matricula  = matricula;
+		compMat(matricula);
 		this.marca = marca;
 		this.color = color;
+	}
+	
+	private void compMat(String matricula)
+	{
+		int i=0;
+		boolean cor= true;
+		while(i<4 && cor)
+		{
+			char c = matricula.charAt(i);
+			if (c >=0 && c<=9)
+				i++;
+			else
+				cor = false;	
+		}
+		while(i<matricula.length() && cor)
+		{
+			char c = matricula.charAt(i);
+			if ((c>= 'a' && c<= 'z') || (c>='A' && c<='Z'))
+				i++;
+			else
+				cor = false;
+		}
+		
+		if (cor)
+			this.matricula=matricula;
+		else
+			this.matricula="ABCD123";
 	}
 	public abstract void rueda(Ruedas r);
 }	
